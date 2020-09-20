733039e611ba7f2456dd37760d38e25fe3edddc6
Raian Rahman <37942208+Raian-Rahman@users.noreply.github.com>
2020-09-21 02:00:44 +0600

    html and css done

---
+++ code.js
@@ -0,0 +1,27 @@
+console.log("hello world!")
+
+//chache the dump 
+
+//the variable 
+const userScore = 0;
+const compScore = 0;
+
+//getting the value
+//chaching the dom
+//mainly storing the data for further use 
+const userScore_span = document.getElementById('user-score'); //html varaiable that stores dumb element
+const compScore_span = document.getElementById('comp-score'); //same 
+
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result-message");
+
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissor_div = document.getElementById("scissor");
+
+
+console.log(userScore_span);
+console.log(userScore_span);
+console.log(scoreBoard_div);
+console.log(result_div);
+
